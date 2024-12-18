e2916cfd1f738626f612715dd0cc52a79d3f2ef6
Austin <austin@myinstantcare.com>
2024-12-18 15:03:51 -0800

    back to completeing part 1

--- Day_6/Puzzle 2/main.zig
+++ Day_6/Puzzle 2/main.zig
@@ -1,199 +1,240 @@
 const std = @import("std");
-const err = error.OutOfBounds;
+
+const INPUT_BUFFER_SIZE = 1048576;
+
+const Direction = enum {
+    N,
+    S,
+    E,
+    W,
+    NE,
+    NW,
+    SE,
+    SW,
+};
+const XY = struct {
+    x: usize,
+    y: usize,
+};
 
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
-    const file_name = "test_input.txt";
-    var result: u16 = 0;
+    const input_file = "input.txt";
+    const map_init: CharMap = try .init(input_file);
+    var map = map_init;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
 
-    const content = try read_file(allocator, file_name);
-    var original_board = try build_board(allocator, content);
+    const start: usize = map.getChar('^').?;
+    var guard: Guard = try .init(.N, start, '^', allocator);
+    try guard.patrol(&map);
+    std.debug.print("locations visited: {d}\n", .{guard.visited.count()});
+}
 
-    for (original_board, 0..) |line, i| {
-        for (line, 0..) |elem, j| {
-            if (elem == '.') {
-                var counter: u16 = 0;
-                var board = try copy_board(allocator, original_board);
-                board[i][j] = 'O';
-                var old_spaces_count: u16 = 0;
-                while (contains_gaurd(board)) {
-                    board = new_board(board) catch remove_gaurd(board);
-                    if (count_spaces(board) == old_spaces_count) {
-                        counter += 1;
-                    } else if (counter > 50) {
-                        result += 1;
-                        break;
-                    }
-                    {
-                        old_spaces_count = count_spaces(board);
-                    }
+const CharMap = struct {
+    grid_string: [INPUT_BUFFER_SIZE]u8 = undefined,
+    grid_string_len: usize = undefined,
+    width: usize = undefined,
+    height: usize = undefined,
+
+    const Self = @This();
+
+    pub fn init(input_path: []const u8) !Self {
+        var self: Self = .{};
+        var file = try std.fs.cwd().openFile(input_path, .{});
+        defer file.close();
+        self.grid_string_len = try file.read(&self.grid_string);
+        std.debug.print("file length is {d}\n", .{self.grid_string_len});
+        self.width = std.mem.indexOf(u8, &self.grid_string, "\n").? + 1;
+        std.debug.print("width of each line is {d}\n", .{self.width});
+        self.height = self.grid_string_len / self.width + 1;
+        std.debug.print("height is {d}\n", .{self.height});
+        return self;
+    }
+
+    pub fn init_slice(input: []const u8) Self {
+        var self: Self = .{};
+        std.mem.copyForwards(u8, &self.grid_string, input);
+        self.grid_string_len = std.mem.indexOf(u8, &self.grid_string, "\x00").?;
+        self.width = std.mem.indexOf(u8, &self.grid_string, "\n").? + 1;
+        self.height = self.grid_string_len.self.width;
+        return self;
+    }
+
+    pub fn getXY(self: *Self, i: usize) XY {
+        return .{ .x = i % self.width, .y = i / self.width };
+    }
+
+    pub fn indexFromXY(self: *Self, xy: XY) usize {
+        return xy.y * self.width + xy.x;
+    }
+
+    pub fn getChar(self: *Self, needle: u8) ?usize {
+        for (self.grid_string, 0..) |c, i| {
+            if (needle == c) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    pub fn checkDirection(self: *Self, i: usize, dir: Direction) ?usize {
+        var xy = self.getXY(i);
+        switch (dir) {
+            .N => {
+                if (xy.y > 0) xy.y -= 1 else return null;
+            },
+            .S => {
+                if (xy.y < self.height - 1) {
+                    xy.y += 1;
+                } else {
+                    std.debug.print("failed because location ({d}, {d}) is less than height {d}\n", .{ xy.x, xy.y, self.height });
+                    return null;
                 }
-                print_board(board);
+            },
+            .E => {
+                if (xy.x < self.width - 2) xy.x += 1 else return null;
+            },
+            .W => {
+                if (xy.x > 0) xy.x -= 1 else return null;
+            },
+            else => unreachable,
+        }
+        return self.indexFromXY(xy);
+    }
+};
+
+const Guard = struct {
+    direction: Direction,
+    location: usize,
+    char: u8,
+    visited: std.AutoArrayHashMap(usize, u32),
+
+    const Self = @This();
+
+    pub fn init(direction: Direction, location: usize, char: u8, allocator: std.mem.Allocator) !Self {
+        var self: Self = .{ .direction = direction, .location = location, .char = char, .visited = .init(allocator) };
+        try self.visited.put(location, 1);
+        return self;
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.visited.deinit();
+    }
+
+    pub fn turn(self: *Self) void {
+        switch (self.direction) {
+            .N => {
+                self.char = '>';
+                self.direction = Direction.E;
+            },
+            .E => {
+                self.char = 'v';
+                self.direction = Direction.S;
+            },
+            .S => {
+                self.char = '<';
+                self.direction = Direction.W;
+            },
+            .W => {
+                self.char = '^';
+                self.direction = Direction.N;
+            },
+            else => unreachable,
+        }
+    }
+
+    pub fn patrol(self: *Self, map: *CharMap) !void {
+        while (try self.step(map)) {
+            std.debug.print("{s}\n", .{map.grid_string});
+        }
+    }
+
+    pub fn step(self: *Self, map: *CharMap) !bool {
+        const next_i: usize = if (map.checkDirection(self.location, self.direction)) |i| i else {
+            std.debug.print("failed map checkDirection at location: {d} and direction {any}\n", .{ self.location, self.direction });
+            return false;
+        };
+
+        if (map.grid_string[next_i] == '#') {
+            self.turn();
+            map.grid_string[self.location] = self.char;
+        } else {
+            map.grid_string[self.location] = 'X';
+            self.location = next_i;
+            const result = try self.visited.getOrPut(self.location);
+            if (!result.found_existing) {
+                result.value_ptr.* = 0;
             }
-            original_board = try build_board(allocator, content);
+            result.value_ptr.* += 0;
+            map.grid_string[self.location] = self.char;
         }
+        return true;
     }
+};
 
-    std.debug.print("Result: {d}\n", .{result});
-}
+pub const InputIterator = struct {
+    _buffer: [1048576]u8 = undefined,
+    _bufLen: usize = 0,
+    lines: std.mem.TokenIterator(u8, .any) = undefined,
+    split: std.mem.SplitIterator(u8, .any) = undefined,
 
-// get content from file
-fn read_file(allocator: std.mem.Allocator, path: []const u8) ![]const u8 {
-    var file = try std.fs.cwd().openFile(path, .{});
-    defer file.close();
-    const file_len = try file.getEndPos();
-    const content = try file.readToEndAlloc(allocator, file_len);
-    return content;
-}
+    const Self = @This();
 
-fn build_board(allocator: std.mem.Allocator, input: []const u8) ![][]u8 {
-    var board = std.ArrayList([]u8).init(allocator);
-    defer board.deinit();
-    var row = std.ArrayList(u8).init(allocator);
-    defer row.deinit();
-
-    var line = std.mem.tokenizeAny(u8, input, "\n\r");
-    while (line.peek() != null) {
-        for (line.next().?) |elem| {
-            try row.append(elem);
+    pub fn init(path: []const u8) !Self {
+        var self = Self{};
+        for (self._buffer, 0..) |_, i| {
+            self._buffer[i] = '\x00';
         }
-        try board.append(try row.toOwnedSlice());
+        var file = try std.fs.cwd().openFile(path, .{});
+        defer file.close();
+        self._bufLen = try file.read(&self._buffer);
+        self.tokenizeLines();
+        return self;
     }
 
-    return try board.toOwnedSlice();
-}
-
-fn print_board(board: [][]u8) void {
-    for (board) |line| {
-        std.debug.print("{s}\n", .{line});
-    }
-}
-
-fn new_board(board: [][]u8) ![][]u8 {
-    for (board, 0..) |line, i| {
-        for (line, 0..) |elem, j| {
-            switch (elem) {
-                '<' => {
-                    // std.debug.print("moving left...\n", .{});
-                    if (j == 0) {
-                        // std.debug.print("Game Over!!!\n", .{});
-                        return remove_gaurd(board);
-                    } else {
-                        const valid = try valid_move(board, j - 1, i);
-                        if (valid) {
-                            // std.debug.print("good to move...left\n", .{});
-                            return move_gaurd(board, j, i, j - 1, i);
-                        } else {
-                            return rotate_gaurd(board, j, i);
-                        }
-                    }
-                },
-                '^' => {
-                    // std.debug.print("moving up...\n", .{});
-                    if (i == 0) {
-                        // std.debug.print("Game Over!!!\n", .{});
-                        return remove_gaurd(board);
-                    } else {
-                        const valid = try valid_move(board, j, i - 1);
-                        if (valid) {
-                            // std.debug.print("good to move...up\n", .{});
-                            return move_gaurd(board, j, i, j, i - 1);
-                        } else {
-                            return rotate_gaurd(board, j, i);
-                        }
-                    }
-                },
-                '>' => {
-                    // std.debug.print("moving right...\n", .{});
-                    const valid = try valid_move(board, j + 1, i);
-                    if (valid) {
-                        // std.debug.print("good to move...right\n", .{});
-                        return move_gaurd(board, j, i, j + 1, i);
-                    } else {
-                        return rotate_gaurd(board, j, i);
-                    }
-                },
-                'v' => {
-                    // std.debug.print("moving down...\n", .{});
-                    const valid = try valid_move(board, j, i + 1);
-                    if (valid) {
-                        // std.debug.print("good to move...down\n", .{});
-                        return move_gaurd(board, j, i, j, i + 1);
-                    } else {
-                        return rotate_gaurd(board, j, i);
-                    }
-                },
-                else => {},
-            }
+    pub fn init_(path: []const u8) !Self {
+        var self = Self{};
+        for (self._buffer, 0..) |_, i| {
+            self._buffer[i] = '\x00';
         }
+        var file = try std.fs.cwd().openFile(path, .{});
+        defer file.close();
+        self._bufLen = try file.read(&self._buffer);
+        _ = self.splitLines();
+        return self;
     }
-    return board;
-}
 
-fn valid_move(board: [][]u8, x: usize, y: usize) !bool {
-    if (x >= board[0].len or y >= board.len) {
-        return error.OutOfBounds;
-    } else if (board[y][x] == '#' or board[y][x] == 'O') {
-        return false;
-    } else return true;
-}
-
-fn move_gaurd(board: [][]u8, x_old: usize, y_old: usize, x_new: usize, y_new: usize) [][]u8 {
-    board[y_new][x_new] = board[y_old][x_old];
-    board[y_old][x_old] = 'X';
-
-    return board;
-}
-
-fn rotate_gaurd(board: [][]u8, x: usize, y: usize) [][]u8 {
-    switch (board[y][x]) {
-        '<' => board[y][x] = '^',
-        '^' => board[y][x] = '>',
-        '>' => board[y][x] = 'v',
-        'v' => board[y][x] = '<',
-        else => {},
+    pub fn tokenizeLines(self: *Self) void {
+        self.lines = std.mem.tokenizeAny(u8, &self._buffer, "\n\x00");
     }
-    return board;
-}
 
-fn remove_gaurd(board: [][]u8) [][]u8 {
-    for (board, 0..) |line, i| {
-        for (line, 0..) |elem, j| {
-            if (elem == '<' or elem == '^' or elem == '>' or elem == 'v') {
-                board[i][j] = 'X';
-            }
+    pub fn splitLines(self: *Self) void {
+        self.split = std.mem.splitAny(u8, &self._buffer, "\n\x00");
+    }
+
+    pub fn next(self: *Self) ?[]const u8 {
+        if (@intFromPtr(&self.lines.buffer[0]) != @intFromPtr(&self._buffer[0])) self.lines.buffer = self._buffer[0..];
+        if (self.lines.index >= self._bufLen - 1) {
+            return null;
         }
-    }
-    return board;
-}
+        const result = self.lines.peek() orelse {
+            return null;
+        };
+        if (result.len == 0) return null;
 
-fn contains_gaurd(board: [][]u8) bool {
-    for (board) |line| {
-        for (line) |elem| {
-            if (elem == '<' or elem == '^' or elem == '>' or elem == 'v') {
-                return true;
-            }
+        return self.lines.next();
+    }
+
+    pub fn next_s(self: *Self) ?[]const u8 {
+        if (@intFromPtr(&self.split.buffer[0]) != @intFromPtr(&self._buffer[0])) self.split.buffer = self._buffer[0..];
+        if (self.split.index.? >= self._bufLen - 1) {
+            return null;
         }
-    }
-    return false;
-}
 
-fn count_spaces(board: [][]u8) u16 {
-    var count: u16 = 0;
-    for (board) |line| {
-        for (line) |elem| {
-            if (elem == 'X') {
-                count += 1;
-            }
-        }
+        return self.split.next();
     }
 
-    return count;
-}
-
-fn copy_board(allocator: std.mem.Allocator, board: [][]u8) ![][]u8 {
-    const copied_board = try allocator.alloc([]u8, board.len);
-    @memcpy(copied_board, board);
-
-    return copied_board;
-}
+    pub fn reset(self: *Self) void {
+        self.lines.reset();
+    }
+};
