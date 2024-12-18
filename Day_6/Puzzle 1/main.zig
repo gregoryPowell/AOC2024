f09f073d40755a46172d80216c6698f29ac3f1e4
Austin <austin@myinstantcare.com>
2024-12-17 18:31:17 -0800

    basic movement working

--- Day_6/Puzzle 1/main.zig
+++ Day_6/Puzzle 1/main.zig
@@ -7,11 +7,11 @@ pub fn main() !void {
     const result: u32 = 0;
 
     const content = try read_file(allocator, file_name);
-    const board = try build_board(allocator, content);
+    var board = try build_board(allocator, content);
     print_board(board);
-    _ = move_gaurd(board) catch {
-        std.debug.print("game over gaurd left board! \n", .{});
-    };
+    for (0..6) |_| {
+        board = try new_board(board);
+    }
 
     std.debug.print("Result: {d}\n", .{result});
 }
@@ -25,40 +25,60 @@ fn read_file(allocator: std.mem.Allocator, path: []const u8) ![]const u8 {
     return content;
 }
 
-fn build_board(allocator: std.mem.Allocator, input: []const u8) ![][]const u8 {
-    var board = std.ArrayList([]const u8).init(allocator);
+fn build_board(allocator: std.mem.Allocator, input: []const u8) ![][]u8 {
+    var board = std.ArrayList([]u8).init(allocator);
     defer board.deinit();
+    var row = std.ArrayList(u8).init(allocator);
+    defer row.deinit();
 
     var line = std.mem.tokenizeAny(u8, input, "\n\r");
     while (line.peek() != null) {
-        try board.append(line.next().?);
+        for (line.next().?) |elem| {
+            try row.append(elem);
+        }
+        try board.append(try row.toOwnedSlice());
     }
 
     return try board.toOwnedSlice();
 }
 
-fn print_board(board: [][]const u8) void {
+fn print_board(board: [][]u8) void {
     for (board) |line| {
         std.debug.print("{s}\n", .{line});
     }
 }
 
-fn move_gaurd(board: [][]const u8) ![][]const u8 {
+fn new_board(board: [][]u8) ![][]u8 {
+    var return_board: [][]u8 = undefined;
     for (board, 0..) |line, i| {
         for (line, 0..) |elem, j| {
             switch (elem) {
                 '<' => {
                     std.debug.print("moving left...\n", .{});
-                    const valid = try valid_move(board, j - 1, i);
-                    if (valid) {
-                        std.debug.print("good to move...left", .{});
+                    if (j == 0) {
+                        std.debug.print("Game Over!!!\n", .{});
+                    } else {
+                        const valid = try valid_move(board, j - 1, i);
+                        if (valid) {
+                            std.debug.print("good to move...left", .{});
+                            return_board = move_gaurd(board, j, i, j - 1, i);
+                        } else {
+                            return_board = rotate_gaurd(board, j, i);
+                        }
                     }
                 },
                 '^' => {
                     std.debug.print("moving up...\n", .{});
-                    const valid = try valid_move(board, j, i - 1);
-                    if (valid) {
-                        std.debug.print("good to move...up\n", .{});
+                    if (i == 0) {
+                        std.debug.print("Game Over!!!\n", .{});
+                    } else {
+                        const valid = try valid_move(board, j, i - 1);
+                        if (valid) {
+                            std.debug.print("good to move...up\n", .{});
+                            return_board = move_gaurd(board, j, i, j, i - 1);
+                        } else {
+                            return_board = rotate_gaurd(board, j, i);
+                        }
                     }
                 },
                 '>' => {
@@ -66,32 +86,51 @@ fn move_gaurd(board: [][]const u8) ![][]const u8 {
                     const valid = try valid_move(board, j + 1, i);
                     if (valid) {
                         std.debug.print("good to move...right\n", .{});
+                        return_board = move_gaurd(board, j, i, j + 1, i);
+                    } else {
+                        return_board = rotate_gaurd(board, j, i);
                     }
                 },
-                'V' => {
+                'v' => {
                     std.debug.print("moving down...", .{});
                     const valid = try valid_move(board, j, i + 1);
                     if (valid) {
                         std.debug.print("good to move...down\n", .{});
+                        return_board = move_gaurd(board, j, i, j, i + 1);
+                    } else {
+                        return_board = rotate_gaurd(board, j, i);
                     }
                 },
                 else => {},
             }
         }
     }
-    return board;
+    print_board(return_board);
+    return return_board;
 }
 
-fn valid_move(board: [][]const u8, x: usize, y: usize) !bool {
-    if (x < 0 or x >= board[0].len or y < 0 or y >= board.len) {
+fn valid_move(board: [][]u8, x: usize, y: usize) !bool {
+    if (x >= board[0].len or y >= board.len) {
         return error.OutOfBounds;
     } else if (board[y][x] == '#') {
         return false;
     } else return true;
 }
 
-fn move_gaurd(board: [][]const u8, x_old: usize, y_old: usize, x_new: usize, y_new: usize) [][]const u8 {
-    board[y_new][x_new] = board[x]
+fn move_gaurd(board: [][]u8, x_old: usize, y_old: usize, x_new: usize, y_new: usize) [][]u8 {
+    board[y_new][x_new] = board[y_old][x_old];
+    board[y_old][x_old] = 'X';
 
+    return board;
+}
+
+fn rotate_gaurd(board: [][]u8, x: usize, y: usize) [][]u8 {
+    switch (board[y][x]) {
+        '<' => board[y][x] = '^',
+        '^' => board[y][x] = '>',
+        '>' => board[y][x] = 'v',
+        'v' => board[y][x] = '<',
+        else => {},
+    }
     return board;
 }
