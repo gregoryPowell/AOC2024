4375db58d53f33e9b2ebdbb64e05362ac1b5bc0f
Austin <austin@myinstantcare.com>
2024-12-23 01:25:48 -0500

    completed

--- Day_9/Puzzle 1/main.zig
+++ Day_9/Puzzle 1/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const file_path = "test_input.txt";
+const file_path = "input.txt";
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -9,10 +9,10 @@ pub fn main() !void {
     var file = try std.fs.cwd().openFile(file_path, .{});
     defer file.close();
     const content = try file.readToEndAlloc(allocator, try file.getEndPos());
-    std.debug.print("{s}", .{content});
+    // std.debug.print("{s}", .{content});
 
     var index: usize = 0;
-    var original_file_system = std.ArrayList(u8).init(allocator);
+    var original_file_system = std.ArrayList(u64).init(allocator);
     defer original_file_system.deinit();
 
     while (index < content.len) : (index += 2) {
@@ -25,51 +25,51 @@ pub fn main() !void {
 
         try original_file_system.appendSlice(try new_disk.file_array.toOwnedSlice());
     }
-    std.debug.print("{s}\n", .{original_file_system.items});
+    // std.debug.print("{d}\n", .{original_file_system.items});
     var file_system = try original_file_system.toOwnedSlice();
 
     for (file_system, 0..) |elem, i| {
-        if (elem == '.') {
+        if (elem == 999999) {
             var j = file_system.len - 1;
             while (j >= i) : (j -= 1) {
-                if (file_system[j] != '.') {
+                if (file_system[j] != 999999) {
                     file_system[i] = file_system[j];
-                    file_system[j] = '.';
+                    file_system[j] = 999999;
                     break;
                 }
             }
             if (j <= i) break;
-            std.debug.print("{s}\n", .{file_system});
+            // std.debug.print("{d}\n", .{file_system});
         }
     }
 
     var checksum: u64 = 0;
     for (file_system, 0..) |elem, i| {
-        if (elem != '.') {
-            checksum += ((elem - 0x30) * i);
+        if (elem != 999999) {
+            checksum += (elem * i);
         }
     }
     std.debug.print("checksum is {d}\n", .{checksum});
 }
 
 const Disk = struct {
-    id: u8 = undefined,
+    id: usize = undefined,
     files: u8 = undefined,
     free_space: u8 = undefined,
-    file_array: std.ArrayList(u8) = undefined,
+    file_array: std.ArrayList(u64) = undefined,
 
     pub fn init(allocator: std.mem.Allocator, id: usize, files: u8, free_space: u8) !Disk {
         var disk: Disk = undefined;
-        disk.id = @intCast(id);
+        disk.id = id;
         disk.files = files - 0x30;
         disk.free_space = free_space - 0x30;
-        disk.file_array = std.ArrayList(u8).init(allocator);
+        disk.file_array = std.ArrayList(u64).init(allocator);
 
         for (0..disk.files) |_| {
-            try disk.file_array.append(disk.id + 0x30);
+            try disk.file_array.append(disk.id);
         }
         for (0..disk.free_space) |_| {
-            try disk.file_array.append('.');
+            try disk.file_array.append(999999);
         }
 
         return disk;
